494b75a785aa45aa6d4a25649f97c2c6a1c7c785
Evgenuu Sakin <shve-ster@yandex.ru>
2022-10-14 19:57:40 +0300

    eight semi

---
+++ task54/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int [,] GetArray (int m, int n)
+{
+    int [,] newArray = new int [m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray [i, j] = new Random ().Next (1,10);
+        }
+    }
+    return newArray;
+}
+
+void PrintArray (int [,] newArray)
+{
+    for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+        {
+            Console.Write ($" {newArray [i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sort (int [,] newArray)
+{
+    for (int k = 0; k < newArray.GetLength(0); k++)
+    {
+        for (int i = 0; i < newArray.GetLength(1); i++)
+        {
+            int indexMax = i;
+            for (int j = i + 1; j < newArray.GetLength(1); j++)   
+            {
+                if (newArray [k, j] > newArray [k, indexMax])
+                indexMax = j;
+            }
+            int x = newArray [k, i];                          
+            newArray [k, i] = newArray [k, indexMax];                
+            newArray [k, indexMax] = x;
+        } 
+    }
+}
+
+try
+{
+    Console.Write ("Введите количество строк: ");
+    int m = Convert.ToInt32 (Console.ReadLine ());
+    Console.Write ("Введите количество столбцов: ");
+    int n = Convert.ToInt32 (Console.ReadLine ());
+
+    int [,] array = GetArray (m, n);
+    PrintArray (array);
+    Console.WriteLine();
+    Sort (array);
+    PrintArray (array);
+}
+catch
+{
+   Console.Write ("Неверно введены данные!");
+}
